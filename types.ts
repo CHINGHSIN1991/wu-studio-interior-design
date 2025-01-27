2f40247d9cd0e4460a4fc4498c3d4196e81fe30f
Ching-Hsin Chen <chinghsinchen1991@gmail.com>
2025-01-27 13:23:38 +0800

    feat: Modify type declaration

--- types.ts
+++ types.ts
@@ -1,7 +1,27 @@
 export type ProjectCategory = 'home' | 'business'
 export type MeasureUnit = 'm' | 'tp'
 
+export interface ProjectFrontmatter {
+  layout: string;
+  title: string;
+  date: string;
+  author: string;
+  image: {
+    src: string;
+    alt: string;
+  };
+  description: string;
+  draft: boolean;
+  category: string;
+}
+
 export interface Project {
+  frontmatter: ProjectFrontmatter;
+  url: string;
+  content: string;
+}
+
+export interface Temp {
   title: string,
   category: ProjectCategory,
   description: string,
