452adec952a37344cbd6ce5718d98fc07ddecbce
Ching-Hsin Chen <chinghsinchen1991@gmail.com>
2025-01-27 13:24:01 +0800

    feat: Add filter feature to project posts

--- src/ts/utils.ts
+++ src/ts/utils.ts
@@ -1,3 +1,5 @@
+import type { Project } from "../../types";
+
 export function slugify(text: string) {
   return text
     .toString()
@@ -13,4 +15,41 @@ export function formatDate(date: Date) {
   return new Date(date).toLocaleDateString('en-US', {
     timeZone: "UTC",
   })
+}
+
+export function formatProjectPost(posts: Project[], {
+  filterOutDrafts = true,
+  filterOutFuturePosts = true,
+  sortByDate = true,
+  limit = undefined as number | undefined,
+} = {}) {
+  const filteredPosts = posts.reduce((acc, post) => {
+    const { date, draft } = post.frontmatter;
+
+    // filterOutDrafts if true
+    if (filterOutDrafts && draft) return acc;
+
+    // filterOutFuturePosts if true
+    if (filterOutFuturePosts && new Date(date) > new Date()) return acc;
+    // add post to acc
+    acc.push(post);
+    return acc;
+  }, [] as Project[])
+  // sort by date or randomize
+  if (sortByDate) {
+    filteredPosts.sort((a, b) => {
+      const dateA = new Date((a as Project).frontmatter.date).getTime();
+      const dateB = new Date((b as Project).frontmatter.date).getTime();
+      return dateB - dateA;
+    });
+  } else {
+    filteredPosts.sort(() => Math.random() - 0.5);
+  }
+
+  // limit if number is passed
+  if (typeof limit === "number") {
+    return filteredPosts.slice(0, limit);
+  }
+
+  return filteredPosts;
 }
