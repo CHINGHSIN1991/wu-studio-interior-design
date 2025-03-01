6d063443aa30cc4a62f956021255e9d3b056e7ef
Ching-Hsin Chen <chinghsinchen1991@gmail.com>
2025-03-01 12:59:15 +0800

    style: Modify link style

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,12 @@
 import { defineConfig } from 'unocss'
 
 export default defineConfig({
-  // ...UnoCSS options
+  theme: {
+    colors: {
+      primary: {
+        DEFAULT: '#3b82f6',
+        dark: '#2563eb'
+      }
+    }
+  }
 })
